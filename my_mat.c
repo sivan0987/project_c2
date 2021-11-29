2dc56b5fd401f119c5ca966a4f40cd73a9b70130
sivan0987 <93656677+sivan0987@users.noreply.github.com>
2021-11-29 18:47:34 +0200

    Add files via upload

--- my_mat.c
+++ my_mat.c
@@ -1,46 +1,66 @@
-#define siz 10
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
 #include "my_mat.h"
-int mat[siz][siz];
+#include <stdlib.h>
+#define N 10
 
-void A ( ){
-    int num;
-    for(int k=0 ; k<siz ; k++){
-        for(int t=0 ; t<siz ; t++){
-            scanf("%d",&num);
-            mat[k][t]=num;
-        }
+// ***** function A *****
+void  a()
+{
+int x=0;
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        scanf("%d ",&x);
+        mat[i][j]=x;
     }
 }
-int temp(int i , int j){
-    int mat2[10][10];
-    for (int k = 0; k < siz; k++) {
-    for (int i = 0; i < siz; i++) {
-      for (int j = 0; j < siz; j++) {
-        if (mat[i][k] + mat[k][j] < mat[i][j]){
-                if(mat[i][k] + mat[k][j] !=0 ){
-                       mat2[i][j] = mat[i][k] + mat[k][j];}
-                       }
-      }}}
-    if(mat2[i][j]==0)return -1;
-    return mat2[i][j];
 }
-int C (){
-    int i,j;
-    scanf("%d", &i);
-    scanf("%d", &j);
-    int ans = temp(i,j);
-    return ans;
-
+int C_help(int x, int y){
+    int a[N][N];
+    for (int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            a[i][j] = mat[i][j];
+            if(i!=j&&a[i][j]==0)
+            {
+                a[i][j]=999999;
+            }
+            else if(i==j)
+             a[i][j]=0;
+        }   
+    }
+   for (int k = 0; k < N; k++) {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        if (a[i][k] + a[k][j] < a[i][j])
+            a[i][j] = a[i][k] + a[k][j];
+      }
+    }
+  }
+if (a[x][y]==0 || a[x][y]>=999999)
+return -1;
+ return a[x][y];
 }
-void B(){
+// // // ***** function B *****
+void b()
+{
     int i,j;
-    scanf("%d", &i);
-    scanf("%d", &j);
-    int ans=temp(i,j);
-    if(ans==-1)
-        printf("False\n");
+    scanf("%d",&i);
+    scanf("%d",&j);
+    if(C_help(i,j)!=-1 )
+       printf("True\n");  
     else
-        printf("True\n");
+       printf("False\n");
 }
-
+void c()
+{
+   int i,j;
+    scanf("%d",&i);
+    scanf("%d",&j);
+     int a = C_help(i,j);
+     printf("%d\n",a);
+}
