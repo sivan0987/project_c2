0195f0a8389bd35850a14425de82f420e050abd5
sivan0987 <93656677+sivan0987@users.noreply.github.com>
2021-11-30 00:31:32 +0200

    Add files via upload

--- my_mat.c
+++ my_mat.c
@@ -22,21 +22,25 @@ int temp(int x, int y){
         for(int j = 0; j < N; j++){
 
             a[i][j] = mat[i][j];
-            if(i!=j&&a[i][j]==0)
-            {
-                a[i][j]=999999;
-            }
-            else if(i==j)
-             a[i][j]=0;
+            // if(i!=j&&a[i][j]==0)
+            // {
+            //     a[i][j]=999999;
+            // }
+            // else if(i==j)
+            //  a[i][j]=0;
         }   
     }
+    if(a[x][y]==0){
+        for (int k = 0; k < N; k++) {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        if (a[i][k] + a[k][j] > a[i][j]){
+            a[i][j] = a[i][k] + a[k][j];
+    }}}}}
    for (int k = 0; k < N; k++) {
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < N; j++) {
         if (a[i][k] + a[k][j] < a[i][j]){
-            if(a[i][k] + a[k][j]==0)
-                        a[i][j]=a[i][j];
-            else
             a[i][j] = a[i][k] + a[k][j];
             
       }
@@ -44,7 +48,7 @@ int temp(int x, int y){
     }
   }
   //|| a[x][y]>=999999
-if (a[x][y]==0 || a[x][y]>=999999)
+if (a[x][y]==0 )
 return -1;
  return a[x][y];
 }
